8c28bb7057696b607f5befec7ac4f1dabfa505e3
jillmartin <jilliandmartin@gmail.com>
2018-03-24 10:02:08 -0600

    pic url added to users table

--- database/migrations/2018_03_24_153103_add_profilepic_table.php
+++ database/migrations/2018_03_24_153103_add_profilepic_table.php
@@ -13,7 +13,9 @@ class AddProfilepicTable extends Migration
      */
     public function up()
     {
-        //
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('pic_url');
+        });
     }
 
     /**
@@ -23,6 +25,8 @@ class AddProfilepicTable extends Migration
      */
     public function down()
     {
-        //
+        Schema::table('users', function (Blueprint $table) {
+         $table->dropColumn('pic_url');
+        });
     }
 }
